0aa8c9eeeb178c6ddf5f562d3aa7c078d4563cd8
sachin kumar <sachinkumarshera@gmail.com>
2023-10-19 17:20:23 +0530

    ui changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: "http://127.0.0.1:3000",
+  api: "https://api.hamrovivah.in",
   // api: "https://hamro.notesocean.com",
   facebookAppId: "280034274792025",
   googleClientId: "712518121703-sp5t3febjcq462fq40ck5e5m39rgjq5u.apps.googleusercontent.com",
